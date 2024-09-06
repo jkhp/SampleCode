f01f05b93bfd90896040e373322b5e7cfb4b953e
jkhp <n6anqwiq9@naver.com>
2024-09-06 19:50:32 +0900

    Practice problem 3 before

--- StringCtrlSample/StringCtrlSample/StringCtrlSample.cpp
+++ StringCtrlSample/StringCtrlSample/StringCtrlSample.cpp
@@ -4,9 +4,18 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-	CMyString strData;
+	// MyString.h, MyString.cpp을 수정해서 동작하게 만들기
+	CMyString strData, strTest;
 	strData.SetString("Hello");
-	cout << strData.GetString() << endl;
+	strTest.SetString("World");
+
+	//복사 생성
+	CMyString strNewData(strData);
+	cout << strNewData.GetString() << endl;
+
+	// 단순 대입 연산자 호출
+	strNewData = strTest;
+	cout << strNewData.GetString() << endl;
 
 	return 0;
 
